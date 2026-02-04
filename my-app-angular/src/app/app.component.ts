c3a22b37db53c4fd8a638b1715b83831c11e7dbb
dhruva3112 <dhruva01998@gmail.com>
2026-02-04 18:27:06 -0500

    add remove from cart feature

--- my-app-angular/src/app/app.component.ts
+++ my-app-angular/src/app/app.component.ts
@@ -9,55 +9,36 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
   title = 'my-app-angular';
   name = 'Renan';
-
-  imageUrl =
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJffy5BzAqh_fl9LjrAPi-lDsx05wdFQHUjA&s';
-
+  imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJffy5BzAqh_fl9LjrAPi-lDsx05wdFQHUjA&s';
   isLoggedIn = true;
-
-  items = [
-    'Item A',
-    'Item B',
-    'Item C',
-    'Item D',
-    'Item E',
-    'Item F',
-    'Item G'
-  ];
-
+  items = ["Item A", "Item B", "Item C", "Item D", "Item E", "Item F", "Item G"];
   isActive = false;
   textColor = 'red';
   fontSize = 50;
 
-  clickButton() {
-    console.log('Button Clicked!');
-
+  clickButton(){
+    console.log("Button Clicked!");
     this.name = 'Ronaldo';
-
-    this.imageUrl =
-      'https://media.istockphoto.com/id/154401486/photo/button-blue-blank.jpg?s=612x612&w=0&k=20&c=hI3EJRMxiY4UrIXTEKDxObYTkKOg1TusSI7T8TL0SDw=';
-
+    this.imageUrl = 'https://media.istockphoto.com/id/154401486/photo/button-blue-blank.jpg?s=612x612&w=0&k=20&c=hI3EJRMxiY4UrIXTEKDxObYTkKOg1TusSI7T8TL0SDw=';
     this.isActive = true;
     this.textColor = 'yellow';
     this.fontSize--;
   }
 
-
-  logout() {
+  logout(){
     this.isLoggedIn = false;
   }
 
-  login() {
+  login(){
     this.isLoggedIn = true;
   }
-
-
-
-  removeFromCart(index: number) {
-    this.items.splice(index, 1);
+  removeFromCart(index: number){
+  if(!this.isLoggedIn){
+    alert('Please login to remove items');
+    return;
   }
-
+  this.items.splice(index, 1);
+}
 }
